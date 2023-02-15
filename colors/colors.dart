7983a6c9d63353fe59dd89f0d6d314501c35912e
Mecklerin <124550023+Mecklerin@users.noreply.github.com>
2023-02-15 11:50:57 +0530

    Add files via upload

---
+++ colors/colors.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import '';
+
+// Welcome page
+
+TextStyle sign = TextStyle(fontSize: 22, color: Colors.white, fontWeight: FontWeight.bold);
+TextStyle explore = TextStyle(color: Colors.white, fontSize: 15);
+BorderSide Mob = BorderSide(width: 1.5,color: Colors.green);
+BorderSide Mob1 = BorderSide(width: 1.5,color: Colors.green);
+TextStyle Mob2 =TextStyle(color: Colors.white);
+ButtonStyle send = ElevatedButton.styleFrom(backgroundColor: Colors.green, foregroundColor: Colors.black);
+TextStyle clicking = TextStyle(fontSize: 14, color: Colors.black,fontWeight: FontWeight.bold);
+TextStyle terms = TextStyle(color: Colors.blue, fontSize: 12);
+
+// otp page
+
+TextStyle verify = TextStyle(fontSize: 25, color: Colors.black, fontWeight: FontWeight.bold);
+TextStyle digit = TextStyle(fontSize: 16, color: Colors.black);
+TextStyle star  = TextStyle(fontWeight: FontWeight.bold);
+TextStyle star2  = TextStyle(fontWeight: FontWeight.bold);
+TextStyle star3  = TextStyle(fontWeight: FontWeight.bold);
+TextStyle star4  = TextStyle(fontWeight: FontWeight.bold);
+BorderSide o1 = BorderSide(width: 2, color: Colors.green);
+BorderSide o2 = BorderSide(width: 2, color: Colors.green);
+BorderSide o3 = BorderSide(width: 2, color: Colors.green);
+BorderSide o4 = BorderSide(width: 2, color: Colors.green);
+ButtonStyle verbutton = ElevatedButton.styleFrom(backgroundColor: Colors.green, foregroundColor: Colors.black,);
+TextStyle times = TextStyle(color: Colors.blue, fontSize: 18);
+ButtonStyle resend = ElevatedButton.styleFrom(backgroundColor: Colors.black, foregroundColor: Colors.white,);
+
+
+//Home page
